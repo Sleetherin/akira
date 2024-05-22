291147f8be36a0d1e2616b4dc8d2b6d62a82041c
PartaCana <kalthidra@gmail.com>
2024-05-22 15:41:57 +0200

    finished stylizing the web

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,15 +1,16 @@
 import './SearchBar.css';
 
 import React, { Component } from 'react';
+import { GiBastet } from "react-icons/gi";
 
 export class SearchBar extends Component {
   render() {
     return (
        <div className="menu_style">
-            <p className="name_of_app">BASTET</p>
+            <p className="name_of_app">BASTET<GiBastet /></p>
         <div className="searchbar_style">
             <input type="text" placeholder="Search a topic"/>
-            <button>Search</button>
+            <button width="100">Search</button>
         </div>
        </div>
     )
