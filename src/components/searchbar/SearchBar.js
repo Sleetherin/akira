112f56f58eb867401193d882a4194537d8939b28
PartaCana <kalthidra@gmail.com>
2024-06-06 10:39:16 +0200

    changing the style of topic

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -7,7 +7,7 @@ export class SearchBar extends Component {
   render() {
     return (
        <div className="menu_style">
-            <p className="name_of_app">BASTET<GiBastet /></p>
+            <p className="name_of_app">AKIRA<GiBastet /></p>
         <div className="searchbar_style">
             <input type="text" placeholder="Search a topic"/>
             <button className="search_button_style"><GiPaw/></button>
--- src/components/topic/Topic.js
+++ src/components/topic/Topic.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import "./Topic.css";
+
+import React, { Component } from 'react';
 
 export class Topic extends Component {
   render() {
     const { display_name} = this.props.topic;
     return (
-      <div>
+      <div className="topic_style">
          <p>{display_name}</p>
       </div>
     )
